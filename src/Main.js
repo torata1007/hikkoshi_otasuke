429542d7502f0034eecdf483d06b9c62d678a871
torata2000 <tuanzhongyintai@gmail.com>
2024-02-17 21:27:13 +0900

    mainに変更。

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div>
+      <h1>This is the Main Component</h1>
+      {/* Add your main component content here */}
+    </div>
+  );
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import Main from './Main'; // Import your Main component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Main /> {/* Render your Main component */}
   </React.StrictMode>
 );
